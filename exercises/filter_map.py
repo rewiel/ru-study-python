3fe15431f491d7b3e70c216e19f0c9ddde5bd6bb
Aleksandr Vinnikov <aleksandr_vinnikov@epam.com>
2022-10-25 16:06:04 +0300

    filter_map

--- exercises/filter_map.py
+++ exercises/filter_map.py
@@ -15,4 +15,17 @@ class FilterMapExercise:
         :param input_array: Исходный список.
         :return: Отфильтрованный список.
         """
-        pass
+
+        if len(input_array) > 0:
+            input_array = input_array
+        else:
+            raise IndexError
+
+        return_array = []
+
+        for item in input_array:
+            add_to_array, func_result = func(input_array)
+            if add_to_array:
+                return_array.append(func_result)
+
+        return return_array
